7cfc6b7bb9f0ce2bd811d56ff9304e1a403057af
fetsroman <fets.roman@gmail.com>
2020-09-16 14:00:42 +0300

    Add administrate, setting urls in administrate

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,5 @@
+//= link_tree ../images
+//= link_directory ../javascripts
+//= link_directory ../stylesheets
+//= link administrate/application.css
+//= link administrate/application.js
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    resources :urls, only: [:index, :show, :destroy]
+
+    root to: "urls#index"
+  end
+
   get "/:short_url", to: "urls#show"
+
   namespace :api do
     resources :urls, only: [:index, :show, :create, :update, :destroy]
   end
